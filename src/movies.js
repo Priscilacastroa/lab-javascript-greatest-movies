d9dc572a30f844fae714cf90b8d35db9b48059a8
Priscila Castro <priscilacastroa@gmail.com>
2022-03-24 23:00:34 -0300

    incompleto

--- src/movies.js
+++ src/movies.js
@@ -1,26 +1,50 @@
-// The `movies` array from the file `src/data.js`.
-console.log('movies: ', movies);
-
 
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+
+function getAllDirectors(movies) {
+  const mapped = movies.map((movie) => {
+   return movie.director
+})
+return mapped
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+
+  const numMovies = movies.filter((movie) => {
+    return (
+      movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')
+    );
+  });
+  return numMovies.length;
+
+}
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage() {
+
+  
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies) {
+    let dramaScore = movies.filter(movie => 
+    movie.genre.includes("Drama"))
+    return scoresAverage(dramaScore)
+  }
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(movies) {
+    let order = movies.map(movie => movie.title).sort() ;
+    return order.slice(0,20)
+  }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
